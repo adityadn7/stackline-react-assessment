1eae9b4a6d8fa03a1799f0eb38cec12f5bb406ba
Aditya Nagabhushana Devarayasamudram <devaadit@amazon.com>
2024-07-16 05:39:01 -0700

    Debugging data read issues

--- src/App.tsx
+++ src/App.tsx
@@ -11,15 +11,24 @@ const App: React.FC = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch('/stackline_frontend_assessment_data_2021.json');
+                const response = await fetch('./stackline_frontend_assessment_data_2021.json');
+                console.log('Response status:', response.status); // Check response status
+
                 if (!response.ok) {
                     throw new Error('Failed to fetch data');
                 }
-                const data = await response.json();
-                if (data && Array.isArray(data) && data.length > 0) {
-                    setProduct(data[0]); // There is only one product in the array
+
+                const contentType = response.headers.get('content-type');
+                if (contentType && contentType.includes('application/json')) {
+                    const data = await response.json();
+                    console.log('Fetched data:', data); // Check fetched data
+                    if (data && Array.isArray(data) && data.length > 0) {
+                        setProduct(data[0]); // There is only one product in the array
+                    } else {
+                        throw new Error('Product data not found in JSON');
+                    }
                 } else {
-                    throw new Error('Product data not found in JSON');
+                    throw new Error('Response was not JSON');
                 }
             } catch (error) {
                 console.error('Error fetching product data:', error);
@@ -28,6 +37,7 @@ const App: React.FC = () => {
 
         fetchData();
     }, []);
+
 
   return (
       <div className="App">
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Router basename="/stackline-react-assessment">
+            <App />
+        </Router>
     </React.StrictMode>,
     document.getElementById('root')
 );
@@ -15,3 +18,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
